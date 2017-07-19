f18558f1fbd2d05755ccddcc74e436351eb69207
Saran <sarandhanush43@gmail.com>
2017-07-19 12:48:52 +0530

    Objection Creation Examples

---
+++ src/DemoObject/ObjectCreation.java
@@ -0,0 +1,22 @@
+package DemoObject;
+
+public class ObjectCreation {
+	int price = 100;
+	int tax = 5;
+
+	public static void main(String[] args) {
+		ObjectCreation ref = new ObjectCreation();
+		System.out.println(ref.price);
+		System.out.println(ref.tax);
+		ref = new ObjectCreation();
+		System.out.println(ref);
+		System.out.println(ref.price);
+		System.out.println(ref.tax);
+		ref = null;
+		System.out.println(ref);
+		System.out.println(ref.price);
+		System.out.println(ref.tax);
+
+	}
+
+}
